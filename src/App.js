1946b00de1639018c3c9b4e19a3f361b1baa8de7
AlanAlv <alan.alv96@gmail.com>
2020-06-08 04:17:34 -0500

    Added Spinner

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import axios from 'axios';
 import Form from './components/Form';
 import Song from './components/Song';
 import Info from './components/Info';
+import Spinner from './components/Spinner';
 
 function App() {
 
   const [ searchLyrics, saveSearchLyrics ] = useState({});
   const [ lyrics, saveLyrics ] = useState('');
   const [ info, saveInfo ] = useState('');
-  const [ error, saveError ] = useState('');
+  const [ error, saveError ] = useState(false);
+  const [ loading, saveLoading ] = useState(false);
+
 
 
   useEffect(() => {
     if (Object.keys(searchLyrics).length === 0) return;
+    saveLoading(true);
     
     const callAPILyrics = async () => {
       const { artist, song } = searchLyrics;
 
       const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
       const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
+
+      
 
       await Promise.all([axios(url), axios(url2)])
       .then((values) => {
@@ -28,29 +34,38 @@ function App() {
         saveLyrics(lyrics.data.lyrics);
         saveInfo(info.data.artists[0]);
         saveError(false);
+        saveLoading(false);
       })
       .catch((err) => {
         saveError(true);
+        saveLoading(false);
         return;
       });
 
-
-
-
-/* 
-      const [lyrics, info] = await Promise.all([
-        axios.get(url),
-        axios.get(url2)
-      ]);
-
-      saveLyrics(lyrics.data.lyrics);
-      saveInfo(info.data.artists[0]);
- */
-      //saveLyrics(result.data.lyrics);
     }
 
     callAPILyrics();
-  }, [searchLyrics])
+  }, [searchLyrics]);
+
+  const component = (loading) 
+    ? 
+      <Spinner /> 
+    :
+      <Fragment>
+        <div className="col-md-6">
+          <Info 
+            info={info}
+          />
+        </div>
+        <div className="col-md-6">
+          <Song 
+            lyrics={lyrics}
+          />
+        </div>
+      </Fragment>
+  ; 
+
+
   return (
     <Fragment>
       <Form 
@@ -66,18 +81,7 @@ function App() {
                 <h2>Song or Group not found</h2>
               </div>
             :
-              <Fragment>
-                <div className="col-md-6">
-                  <Info 
-                    info={info}
-                  />
-                </div>
-                <div className="col-md-6">
-                  <Song 
-                    lyrics={lyrics}
-                  />
-                </div>
-              </Fragment>
+              component
           }
         </div>
       </div>
